5a14b588f937402be2bbe6b0a77dce0098e44a89
9F4R14 <9faria@gmail.com>
2025-07-17 15:51:31 +0100

    main

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
-#include <stdio.h>
+#include "socketutil.h"
 
 int main(){
 
+    int serverSocketFD = createTCPIpv4Socket();
 
+    struct sockaddr_in *serverAddress = createIPv4Address("",2000);
 
+    int result = bind(serverSocketFD,serverAddress,sizeof(*serverAddress));
+    if(result == 0)
+        printf("socket was bound successfully\n");
+    
+    int listenResult = listen(serverSocketFD,10);
+    
+    struct sockaddr_in clientAddress;
+    int clientAddressSize = sizeof(struct sockaddr_in);
+    int clientSocketFD = accept(serverSocketFD,&clientAddress,&clientAddressSize)
+
+    char* buffer[1024];
+    recv(clientSocketFD,buffer,1024,0);
+
+    printf("Response was %s\n",buffer);
 
     return 0;
 }
